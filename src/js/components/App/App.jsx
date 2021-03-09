ef708b3e9765c278782fdc861864b070c14aec0f
Prashant <prashant07303@gmail.com>
2021-03-09 16:09:03 +0530

    Added dashboard to view saved calculations

--- src/js/components/App/App.jsx
+++ src/js/components/App/App.jsx
@@ -1,9 +1,38 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Select from "../SelectOption/Select";
+import Table from "../Dashboard/Dashboard";
 import { TextInput, Wrapper } from '../Input/TextInput';
 import calculateTax from "../../tax-calculator-helper";
-
+function useSavedCalculationState(defaultValue, key) {
+    const [value, setValue] = React.useState(() => {
+      const savedCalculations = window.localStorage.getItem(key);
+      return savedCalculations !== null
+        ? JSON.parse(savedCalculations)
+        : defaultValue;
+    });
+    React.useEffect(() => {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
+  }
 export const App = () => {
+    // const data = [
+    //     {
+    //       "Country Name": "Afghanistan",
+    //       Capital: "Kabul",
+    //       Currency: "Afghani"
+    //     },
+    //     {
+    //       "Country Name": "Albania",
+    //       Capital: "Tirane",
+    //       Currency: "Lek"
+    //     },
+    //     {
+    //       "Country Name": "Algeria",
+    //       Capital: "Algiers",
+    //       Currency: "Dinar"
+    //     }
+    //   ];     
     const yearOptions = [
         { value: "", label: "Please enter year" },
         { value: "2018", label: "2018" },
@@ -15,14 +44,19 @@ export const App = () => {
     const [income, setIncome] = useState(0);
     const [investment, setInvestment] = useState(0);
     const [finalTaxPayble, setFinalTaxPayble] = useState(0);
-    const [displayTaxCalculator, setDisplayTaxCalculator] = useState(true);
+    const [savedTaxes, setSavedTaxes] = useSavedCalculationState([],"savedtaxes");
     // TODO - need to cehck if useCallback is better than useEfect here?
     const finalTaxCalculation = useEffect(
         () => {
             setFinalTaxPayble(calculateTax(year, age, income, investment));
         },
         [year, age, income, investment],
-    );
+    );  
+    const handleSave = () => {
+        const addSavedTaxes = [...savedTaxes];
+        addSavedTaxes.push({"Year":year,"Age":age,"Income":income,"Investment":investment,"Calculated Tax": finalTaxPayble})
+        setSavedTaxes(addSavedTaxes)
+    }  
     return (
         <>
             {/* <Login ></Login> */}
@@ -45,8 +79,13 @@ export const App = () => {
 
                 </section>
                 <TextInput id="tax" value={finalTaxPayble} readOnly={true}>Final Tax calculated:</TextInput>
+                {/* TODO - We can remove the disabled check, would fail for zero tax scenarios */}
+                <button disabled = {Boolean(finalTaxPayble) === false} onClick= {handleSave}>Save this Calculation</button>
             </>
-
+            <section>
+                <h1>Saved User records</h1>
+            {savedTaxes.length && <Table data={savedTaxes.slice(0,4)} />}
+            </section>
         </>
     )
 }
--- src/js/components/Dashboard/Dashboard.jsx
+++ src/js/components/Dashboard/Dashboard.jsx
@@ -1,2 +1,64 @@
-import styled from 'styled-components';
 import React from "react";
+import styled from "styled-components";
+
+const StyledTable = styled.table`
+  caption-side: top;
+  border: none;
+  border-collapse: collapse;
+  caption-side: bottom; 
+  td,
+  th {
+    border: 1px solid purple;
+  }
+
+  td {
+    padding: 5px 10px;
+  }
+
+  tbody tr {
+    :nth-of-type(odd) {
+      background-color: #efefef;
+    }
+    :hover {
+      background-color: lightpink;
+    }
+  }
+  thead > tr {
+    background-color: #c2c2c2;
+  }
+  caption {
+    font-size: 0.9em;
+    padding: 5px;
+    font-weight: bold;
+  }
+`;
+export default ({ data }) => (
+        <TableMarkup titles={Object.keys(data[0])} data={data} />
+    );
+const TableMarkup = ({ titles, data }) => (
+
+    <StyledTable>
+        <caption></caption>
+        <colgroup>
+            <col />
+            <col />
+            <col />
+        </colgroup>
+        <thead>
+            <tr>
+                {titles.map((title, index) => (
+                    <th key={index}>{title}</th>
+                ))}
+            </tr>
+        </thead>
+        <tbody>
+            {data.map((item, index) => (
+                <tr key={index}>
+                    {titles.map((title, index) => (
+                        <td key={index}>{item[title]}</td>
+                    ))}
+                </tr>
+            ))}
+        </tbody>
+    </StyledTable>
+);
